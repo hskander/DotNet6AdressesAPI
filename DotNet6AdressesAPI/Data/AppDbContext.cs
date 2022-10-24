196f6057c94d5b0ec1021fbd611482bb7554ca5f
HSkander <hamdiskander5@gmail.com>
2022-10-24 17:02:30 +0200

    createAndRegisterDbContextFile

---
+++ DotNet6AdressesAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DotNet6AdressesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet6AdressesAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Address> Address => Set<Address>();
+    }
+}
--- DotNet6AdressesAPI/Program.cs
+++ DotNet6AdressesAPI/Program.cs
@@ -1,3 +1,6 @@
+using DotNet6AdressesAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Register the AppDbContext File
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
