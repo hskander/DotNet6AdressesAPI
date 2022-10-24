6a4d2c4bff08665965c2fe109a63252077d20603
HSkander <hamdiskander5@gmail.com>
2022-10-24 19:29:05 +0200

    imlementPutAndDeleteMethods

--- DotNet6AdressesAPI/Controllers/AddressesController.cs
+++ DotNet6AdressesAPI/Controllers/AddressesController.cs
@@ -32,14 +32,53 @@ namespace DotNet6AdressesAPI.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<List<Address>>> GetAddressById(int id)
+        public async Task<ActionResult<Address>> GetAddressById(int id)
         {
             var address = await this._appDbContext.Addresses.FindAsync(id);
-            if(address == null)
+            if (address == null)
             {
                 return BadRequest("Address Not Found.");
             }
             return Ok(address);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Address>>> DeleteAddressById(int id)
+        {
+            var address = await this._appDbContext.Addresses.FindAsync(id);
+            if (address == null)
+            {
+                return BadRequest("Address Not Found.");
+            }
+            else
+            {
+                 this._appDbContext.Addresses.Remove(address);
+                await this._appDbContext.SaveChangesAsync();
+
+            }
+            return Ok(await this._appDbContext.Addresses.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Address>> EditAddress(Address newAddress)
+        {
+            var address = await this._appDbContext.Addresses.FindAsync(newAddress.Id);
+            if (address == null)
+            {
+                return BadRequest("Address with Id= "+ newAddress.Id +" Not Found.");
+            }
+            else
+            {
+                address.Street= newAddress.Street;
+                address.City= newAddress.City;
+                address.HouseNumber= newAddress.HouseNumber;
+                address.ZipCode= newAddress.ZipCode;
+                address.Country= newAddress.Country;
+                this._appDbContext.Addresses.Update(address);
+                await this._appDbContext.SaveChangesAsync();
+
+            }
+            return Ok(newAddress);
         }
 
 
